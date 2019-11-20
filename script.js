5093a7503ef2c2a29994ff237acd522e506ea4dd
turnejoh <53783073+turnejoh@users.noreply.github.com>
2019-11-20 13:56:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function DoIt
+  let First = document.getElementById("First").value;
+  let Plural = document.getElementById(" Plural").value; 
+  let Place = document.getElementById(" Place") .value;
+  let Adjective = document.getElementById("Adjective").value;
+
+  document.getElementById( " myForm") . innerHTML = " <h3> your name is <spanclass= 'First'>" + First + "" + Plural + " </span></h3><p> This concludes this mad lib.</p>"
+  
+
